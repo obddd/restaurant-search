cfa7e62468fb050d3a2b53316b218245b55a9cfe
Obaid <obaidulhaq20@gmail.com>
2020-06-04 21:37:20 +0500

    added api call function in search screen

--- src/api/yelp.js
+++ src/api/yelp.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export default axios.create({
   baseURL: 'https://api.yelp.com/v3/businesses',
-  header: {
+  headers: {
     Authorization:
       'Bearer 15NiGAib0U9fF0tsplvuk-c-v_uD-3__fn3MkQJdxId34p0g57KY8cK4BVATlu8pAmV3WUzY7TpVkJrCiMDKWxZ3t7n7cA1BTgVv5f0pWs7kmr-OTg1sAU9MGxzZXnYx',
   }
--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,19 +1,30 @@
 import React, { useState } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import SearchBar from '../components/SearchBar';
-import yelp from '../api/yelp'
+import yelp from '../api/yelp';
 
 const SearchScreen = () => {
   const [search, setSearch] = useState('');
+  const [results, setResults] = useState([]);
+  const searchApi = async () => {
+    const response = await yelp.get('/search', {
+      params: {
+        term: search,
+        limit: 50,
+        location: 'New York',
+      },
+    });
+    setResults(response.data.businesses);
+  };
   return (
     <View>
       <SearchBar
         search={search}
         onSearchChange={(newSearch) => setSearch(newSearch)}
-        onSearchSubmit={() => console.log('Submitted!')}
+        onSearchSubmit={() => searchApi()}
       />
       <Text>Search Screen</Text>
-      <Text>{search}</Text>
+      <Text>We have found {results.length} results</Text>
     </View>
   );
 };
